80317d05203e3e8e3f641d5121c3e45b49e38b64
meiriv <meiriv@gmail.com>
2016-09-28 10:10:34 -0400

    spinner improvement

--- assets/js/controllers/ApplicationController.js
+++ assets/js/controllers/ApplicationController.js
@@ -4,11 +4,11 @@
 	angular.module('app').controller('ApplicationController', ApplicationController );
 
 	ApplicationController.$inject = [
-		'$scope', '$rootScope' , '$routeParams' , '$location' , 'config' , 'APP_TEXT' 
+		'$scope', '$rootScope' , '$routeParams' , '$location' , 'config' , 'APP_TEXT' , 'spinFactory'
 	];
 
 	function ApplicationController(
-		$scope, $rootScope , $routeParams , $location , config , APP_TEXT
+		$scope, $rootScope , $routeParams , $location , config , APP_TEXT , spinFactory
 	) {
 		var vm = this;
 		$scope.constants = APP_TEXT ;
@@ -16,5 +16,10 @@
 
 		// set URL configuration
 		config.importHelpers(vm);
+
+		//stop spinner after page is loaded, for some reason it is activated after the view is rendered
+		 $scope.$on('$viewContentLoaded', function(){
+		    spinFactory.stop();
+		  });
 	}
 }());
